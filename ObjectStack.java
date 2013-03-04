b1d75bc163cc516961b3eebe8738e4ef609b77ad
SprklO <katie.ann.riley@hotmail.com>
2013-03-04 01:39:40 -0800

    first commit

---
+++ ObjectStack.java
@@ -0,0 +1,56 @@
+// ObjectStack.java
+
+public class ObjectStack {
+    private Object[] item;
+    private int top;
+    private int size;
+    
+    public ObjectStack() {
+        size = 100;
+        item = new Object[size];
+        top = -1;
+    }
+
+    public ObjectStack(int max) {
+        size = max;
+        item = new Object[size];
+        top = -1;
+    }
+    
+    public boolean isEmpty() {
+        return top == -1;
+    }
+    
+    public boolean isFull() {
+        return top == size-1;
+    }
+    
+    public void clear() {
+        top = -1;
+    }
+    
+    public void push(Object x) {
+        if (isFull()) {
+            System.out.println("Push Runtime Error: Stack Overflow");
+            System.exit(1);
+        }    
+        item[++top] = x;
+    }
+    
+    public Object pop() {
+        if (isEmpty()) {
+            System.out.println("Pop Runtime Error: Stack Underflow");
+            System.exit(1);
+        }
+        return item[top--];
+    }
+    
+ 
+    public Object top() {
+        if (isEmpty()) {
+            System.out.println("Top Runtime Error: Stack Underflow");
+            System.exit(1);
+        }    
+        return item[top];
+    }
+}
